d0600ec39deaa4bc3818636bed577f36b16ebf06
ShulepovDen <shulepovdenis@mail.ru>
2020-12-26 22:01:02 +0300

    add build in ignore, deleate some package and rewrite js script

--- src/script/script.js
+++ src/script/script.js
@@ -5,18 +5,6 @@ function validator(_value) {
     validate() {
       return this.isValid;
     },
-    checkAll(n) {
-      if (n === 0 || n === 1) {
-        this.isName();
-      }
-      if (n === 2) {
-        this.isEmail();
-      }
-      if (n === 3) {
-        this.isValidPassword();
-      }
-      return this;
-    },
     isEmail() {
       if (this.value.match(/^[\w-.]+@[\w-]+.[a-z]{2,4}$/)) {
         this.isValid = true;
@@ -46,17 +34,38 @@ function validator(_value) {
 
 window.onload = function upload() {
   const registryButton = document.querySelector('.btn');
-  let currentElem;
+  const firstName = document.querySelector('#name-first');
+  const lastName = document.querySelector('#name-second');
+  const email = document.querySelector('#email');
+  const password = document.querySelector('#password');
   registryButton.onclick = function registyClick() {
-    for (let i = 0; i < 4; i += 1) {
-      currentElem = document.getElementsByClassName('input')[i];
-      if (!validator(currentElem.value).checkAll(i).validate()) {
-        document.getElementsByClassName('error')[i].style.display = 'inline';
-        currentElem.style.backgroundColor = '#800000';
-      } else {
-        document.getElementsByClassName('error')[i].style.display = 'none';
-        currentElem.style.backgroundColor = '#27282f';
-      }
+    if (!validator(firstName.value).isName().validate()) {
+      document.getElementsByClassName('form-line__name-first__error')[0].style.display = 'inline';
+      firstName.style.backgroundColor = '#800000';
+    } else {
+      document.getElementsByClassName('form-line__name-first__error')[0].style.display = 'none';
+      firstName.style.backgroundColor = '#27282f';
+    }
+    if (!validator(lastName.value).isName().validate()) {
+      document.getElementsByClassName('form-line__name-second__error')[0].style.display = 'inline';
+      lastName.style.backgroundColor = '#800000';
+    } else {
+      document.getElementsByClassName('form-line__name-second__error')[0].style.display = 'none';
+      lastName.style.backgroundColor = '#27282f';
+    }
+    if (!validator(password.value).isValidPassword().validate()) {
+      document.getElementsByClassName('form-line__password__error')[0].style.display = 'inline';
+      password.style.backgroundColor = '#800000';
+    } else {
+      document.getElementsByClassName('form-line__password__error')[0].style.display = 'none';
+      password.style.backgroundColor = '#27282f';
+    }
+    if (!validator(email.value).isEmail().validate()) {
+      document.getElementsByClassName('form-line__email__error')[0].style.display = 'inline';
+      email.style.backgroundColor = '#800000';
+    } else {
+      document.getElementsByClassName('form-line__email__error')[0].style.display = 'none';
+      email.style.backgroundColor = '#27282f';
     }
   };
 };
